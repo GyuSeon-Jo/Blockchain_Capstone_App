3ad2c9c7865cbcbdbf1f0f821c5cb5735d496e99
GyuSeon-Jo <plame114@gmail.com>
2021-04-23 20:48:26 +0900

    first upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+
+export default class App extends React.Component {
+  render() {
+    return <WebView source={{ uri: 'https://www.kyonggiupass.com/login' }} userAgent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36' style={{ marginTop: 20 }} />;
+  }
+}
